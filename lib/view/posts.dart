d43667448aa015c2e833d20dddb53064e2cf4182
Rafał Dziuryk <rafal.dziuryk@siroccomobile.com>
2020-04-17 06:34:59 +0200

    T4 - refresh list

--- lib/view/posts.dart
+++ lib/view/posts.dart
@@ -27,28 +27,63 @@ class _PostPageState extends State<PostPage> {
       create: (context) => postsBloc,
       child: Scaffold(
         body: Builder(builder: (BuildContext context) {
-          return RefreshIndicator(
-            key: indicator,
-            child: BlocBuilder<PostsBloc, PostsState>(
-              builder: (BuildContext context, PostsState state) {
-                if (state is DataObtainedPostsState) {
-                  return ListView.builder(
-                      itemCount: state.posts.length,
-                      itemBuilder: (context, index) {
-                        return ListTile(
-                          title: Text(state.posts[index].title),
+          return BlocListener<PostsBloc, PostsState>(
+            listener: (BuildContext context, PostsState state) {
+              if (state is DownloadingErrorPostsState) {
+                Scaffold.of(context).showSnackBar(SnackBar(
+                  content: Text("Error: ${state.errorMessage}"),
+                ));
+              }
+            },
+            child: RefreshIndicator(
+              key: indicator,
+              child: BlocBuilder<PostsBloc, PostsState>(
+                builder: (BuildContext context, PostsState state) {
+                  if (state is DataObtainedPostsState) {
+                    return ListView.builder(
+                        itemCount: state.posts.length,
+                        itemBuilder: (context, index) {
+                          return ListTile(
+                            title: Text(state.posts[index].title),
+                          );
+                        });
+                  } else if (state is EmptyDataPostsState) {
+                    return LayoutBuilder(
+                      builder: (context, constraint) {
+                        return SingleChildScrollView(
+                          physics: AlwaysScrollableScrollPhysics(),
+                          child: ConstrainedBox(
+                            constraints: BoxConstraints(minHeight: constraint.maxHeight,
+                            minWidth: constraint.maxWidth),
+                            child: Center(child: Text("Brak danych, przeciągnij aby spróbować jeszcze raz")),
+                          ),
                         );
-                      });
-                } else {
-                  return Container();
-                }
+                      },
+                    );
+                  } else if (state is DownloadingPostsState) {
+                    return LayoutBuilder(
+                      builder: (context, constraint) {
+                        return SingleChildScrollView(
+                          physics: AlwaysScrollableScrollPhysics(),
+                          child: ConstrainedBox(
+                            constraints: BoxConstraints(minHeight: constraint.maxHeight,
+                                minWidth: constraint.maxWidth),
+                            child: Center(child: CircularProgressIndicator()),
+                          ),
+                        );
+                      },
+                    );
+                  }else {
+                    return Container();
+                  }
+                },
+              ),
+              onRefresh: () {
+                BlocProvider.of<PostsBloc>(context).add(RefreshEvent());
+                return BlocProvider.of<PostsBloc>(context)
+                    .firstWhere((state) => !(state is DownloadingPostsState));
               },
             ),
-            onRefresh: () {
-              BlocProvider.of<PostsBloc>(context).add(RefreshEvent());
-              return BlocProvider.of<PostsBloc>(context)
-                  .firstWhere((state) => !(state is DownloadingPostsState));
-            },
           );
         }),
       ),
